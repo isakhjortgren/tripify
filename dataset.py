955463fd3aebc3539a6d270fa47ac3b5d9425a35
Victor Bäckman <victorbackman2@gmail.com>
2020-07-26 15:31:58 +0200

    WIP

--- dataset.py
+++ dataset.py
@@ -3,29 +3,34 @@ def convert_audio_files_to_mfcc(path):
     import librosa
     import numpy as np
     import pickle
-
+    import pandas as pd
 
     # TODO: Use the timeseries aspect with an LSTM instead of mean values?
-    # y, sr = librosa.load(path, offset=0)
+    y, sr = librosa.load(path)
+    print('y', y.size)
     sr = 48000
 
-    with open(path, 'rb') as f:
-        data = pickle.load(f)
+    # with open(path, 'rb') as f:
+    #
+    #     data = pickle.load(f)
 
-    y = data[0, :]
-    nbr_voices = y.shape[0] - 1
+    # voice_1 = [1]*len(y)
+    # voice_2 = [0]*len(y)
+    # data = np.array([y, voice_1, voice_2])
 
-    for frame in Y_TIMEFAMES:
-        continue
-        # TODO: Create variable for different timeframes
+    # df = pd.DataFrame(data)
 
-        for voice in range(1, nbr_voices+1):
-            continue
-            # TODO: Round up to 1 for every timeframe
+    # df = df.rolling(window=1000).mean()
+    # print(df.columns, df.shape)
+    # exit(0)
 
+    # y = data[0, :]
+    # nbr_voices = y.shape[0] - 1
 
     mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
     # mfcc = np.mean(mfcc.T, axis=0)
+
+    nbr_timeframes = mfcc.shape[1]
 
     zero_crossing_rate = librosa.feature.zero_crossing_rate(y=y)
     # zero_crossing_rate = np.mean(zero_crossing_rate)
@@ -34,21 +39,22 @@ def convert_audio_files_to_mfcc(path):
     spectral_rolloff = librosa.feature.spectral_rolloff(S=S, sr=sr)
     # spectral_rolloff = np.mean(spectral_rolloff)
 
-    print(mfcc.shape)
-    print(spectral_rolloff.shape)
 
     print('MFCC')
-    print(mfcc)
+    print(mfcc.shape)
+    # print(mfcc)
 
     print('\nZero crossing rate')
-    print(zero_crossing_rate)
+    print(zero_crossing_rate.shape)
+    # print(zero_crossing_rate)
 
     print('\nSpectral rolloff')
-    print(spectral_rolloff)
+    print(spectral_rolloff.shape)
+    # print(spectral_rolloff)
 
     features = mfcc
 
-    print(features.shape)
+    # print(features.shape)
     return features
 
 
--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 from dataset import convert_audio_files_to_mfcc
 
-convert_audio_files_to_mfcc('data/victor_1.m4a')
+convert_audio_files_to_mfcc('data/trimmed/victor_0.wav')
