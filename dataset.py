f93b6cd975f0e31693f763f78bb052a672e26c8c
Victor Bäckman <victorbackman2@gmail.com>
2020-07-26 14:46:45 +0200

    WIP

--- dataset.py
+++ dataset.py
@@ -1,14 +1,41 @@
 
-def convert_audio_files_to_mfcc(file_path):
+def convert_audio_files_to_mfcc(path):
     import librosa
+    import numpy as np
+    import pickle
 
-    y, sr = librosa.load(file_path, offset=0)
+
+    # TODO: Use the timeseries aspect with an LSTM instead of mean values?
+    # y, sr = librosa.load(path, offset=0)
+    sr = 48000
+
+    with open(path, 'rb') as f:
+        data = pickle.load(f)
+
+    y = data[0, :]
+    nbr_voices = y.shape[0] - 1
+
+    for frame in Y_TIMEFAMES:
+        continue
+        # TODO: Create variable for different timeframes
+
+        for voice in range(1, nbr_voices+1):
+            continue
+            # TODO: Round up to 1 for every timeframe
+
 
     mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
+    # mfcc = np.mean(mfcc.T, axis=0)
+
     zero_crossing_rate = librosa.feature.zero_crossing_rate(y=y)
+    # zero_crossing_rate = np.mean(zero_crossing_rate)
 
     S, phase = librosa.magphase(librosa.stft(y))
     spectral_rolloff = librosa.feature.spectral_rolloff(S=S, sr=sr)
+    # spectral_rolloff = np.mean(spectral_rolloff)
+
+    print(mfcc.shape)
+    print(spectral_rolloff.shape)
 
     print('MFCC')
     print(mfcc)
@@ -19,6 +46,9 @@ def convert_audio_files_to_mfcc(file_path):
     print('\nSpectral rolloff')
     print(spectral_rolloff)
 
-    return mfcc
+    features = mfcc
+
+    print(features.shape)
+    return features
 
 
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
 from dataset import convert_audio_files_to_mfcc
 
-# if __name__ == 'main':
-#     print('hej')
-convert_audio_files_to_mfcc('data/victor_0.m4a')
+convert_audio_files_to_mfcc('data/victor_1.m4a')
